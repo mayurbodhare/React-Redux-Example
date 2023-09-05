a74a02b8b1fb901bc181f48b50c347ce317b939c
Mayur Bodhare <mrbodhre7@gmail.com>
2023-09-05 10:34:30 +0530

    initial commit

---
+++ src/components/Bonus.js
@@ -0,0 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { incrementBonus } from '../actions';
+
+function Bonus(  ) {
+
+  const amount = useSelector( state => state.account.amount);
+  const points = useSelector( state => state.bonus.points);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="card">
+      <div className="container">
+        <h4>
+          <b>Bonus Component</b>
+        </h4>
+        <h3>Total Amount : ${amount}</h3>
+        <h3>Total Point : {points}</h3>
+
+        <button onClick={() => dispatch(incrementBonus())}>Increment +</button>
+      </div>
+    </div>
+  );
+}
+
+export default Bonus;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { accountReducer } from './reducers/accounts';
+import { bonusReducer } from './reducers/bonus';
+import { Provider } from 'react-redux';
+
+//store
+const store = createStore(
+  combineReducers({
+    account: accountReducer,
+    bonus: bonusReducer
+  }),
+  applyMiddleware(logger, thunk)
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode >
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>
 );
 
